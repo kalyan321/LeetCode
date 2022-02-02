ace4d447d2007c74ab5ced2465d00099b077966c
kalyan_yadav <kalyanyadav4321@gmail.com>
2022-02-02 20:19:05 +0530

    Time: 227 ms (19.11%), Space: 15 MB (78.16%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -6,34 +6,24 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def mainTreeTraversal(root):
+        def mainTreeTraversal(root, arr):
             if root != None:
-                self.mainTree.append(root.val)
+                arr.append(root.val)
             else:
-                self.mainTree.append(None)
+                arr.append(None)
                 return
-            mainTreeTraversal(root.left)
-            mainTreeTraversal(root.right)
-
-        def subTreeTraversal(root):
-            if root != None:
-                self.subTree.append(root.val)
-            else:
-                self.subTree.append(None)
-                return
-            subTreeTraversal(root.left)
-            subTreeTraversal(root.right)
+            mainTreeTraversal(root.left, arr)
+            mainTreeTraversal(root.right, arr)
+            return arr
         
-        self.mainTree = []
-        self.subTree = []
-        subTreeTraversal(subRoot)
-        mainTreeTraversal(root)
-        n = len(self.mainTree)
-        m = len(self.subTree)
+        sub = mainTreeTraversal(subRoot, [])
+        main = mainTreeTraversal(root, [])
+        n = len(main)
+        m = len(sub)
         i = 0
         j = 0
         while i < n and j < m:
-            if self.mainTree[i] == self.subTree[j]:
+            if main[i] == sub[j]:
                 i += 1
                 j += 1
                 if j == m:
