c3e73b7fb13dcf4a61b2c8e6ec85d120644644a5
kalyan_yadav <kalyanyadav4321@gmail.com>
2022-02-08 12:27:25 +0530

    Time: 416 ms (7.91%), Space: 15.9 MB (16.04%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
@@ -9,19 +9,20 @@ class Solution:
             return 0 <= pos <= 6000 and (pos, back) not in seen and pos not in forbidden
 
         def bfs():
-            stack = [[0,False,0]]
-            count = 1
+            stack = [[0,False]]
+            steps = 0
             while stack:
                 for i in range(len(stack)):
-                    pos, back, steps = stack.pop(0)
+                    pos, back = stack.pop(0)
                     if pos == x:
                         return steps
                     if validpos(pos + a, False):
-                        stack.append([pos + a, False, steps + 1])
+                        stack.append([pos + a, False])
                         seen.add((pos + a, back))
                     if not back and validpos(pos - b, True):
-                        stack.append([pos - b, True, steps + 1])
+                        stack.append([pos - b, True])
                         seen.add((pos - b, True))
+                steps += 1
             return -1
 
         return bfs()
