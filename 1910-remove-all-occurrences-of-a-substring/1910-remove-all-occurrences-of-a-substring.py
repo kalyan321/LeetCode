385c5fa56d15915e1bd7f029620dded94e226073
kalyan_yadav <kalyanyadav4321@gmail.com>
2022-02-11 17:27:36 +0530

    Time: 50 ms (52.12%), Space: 14 MB (76.40%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
@@ -4,8 +4,6 @@ class Solution:
         for i in range(len(s)):
             stack += s[i]
             while len(stack) >= len(part) and stack[len(stack) - len(part) : ] == part:
-                print(stack)
                 stack = stack[ : len(stack) - len(part)]
-                print(stack)
         return "".join(stack)
         
