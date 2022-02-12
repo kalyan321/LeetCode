e07bffbfc3f4154c1b56cd42021fbd1226ade375
kalyan_yadav <kalyanyadav4321@gmail.com>
2022-02-12 10:28:47 +0530

    Time: 4400 ms (5.11%), Space: 27.6 MB (5.10%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
@@ -5,21 +5,15 @@
 #         self.next = next
 class Solution:
     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
-        self.res = []
         def recur(head):
             if head.next is None:
-                self.res.append(0)
                 return [0], [head.val]
             ans, stack = recur(head.next)
             while stack and stack[-1] <= head.val:
                 stack.pop(-1)
             if stack:
-                self.res.append(stack[-1])
-                ans.append(stack[-1])
+                ans = [stack[-1]] + ans
             else:
-                ans.append(0)
-                self.res.append(0)
+                ans = [0] + ans
             return ans, stack + [head.val]
-        s = recur(head)
-        return(s[0][::-1])
-        return self.res[::-1]
+        return recur(head)[0]
