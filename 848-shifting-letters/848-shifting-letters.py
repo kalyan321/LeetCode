ca596bbe754087df7263c2e56c468009de9380a9
kalyan_yadav <kalyanyadav4321@gmail.com>
2022-02-13 20:08:23 +0530

    Time: 1070 ms (55.73%), Space: 24.9 MB (96.28%) - LeetHub

--- 848-shifting-letters/848-shifting-letters.py
+++ 848-shifting-letters/848-shifting-letters.py
@@ -1,14 +1,12 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
-        shifts[-1] %= 26 
-        if shifts[-1] == 0:
-                shifts[-1] = 26
-        for i in range(len(shifts)-2, -1, -1):
-            shifts[i] += shifts[i+1]
+        
+        for i in range(len(shifts)-1, -1, -1):
+            if i != len(shifts) - 1:
+                shifts[i] += shifts[i+1]
             shifts [i] %= 26
             if shifts[i] == 0:
                 shifts[i] = 26
-        print(shifts)
         s = list(s)
         for i in range(len(s)):
             val = ord(s[i]) + shifts[i]
