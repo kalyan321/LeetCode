06ef1bd1957655480a2271361f149e59714ab6f6
kalyan_yadav <kalyanyadav4321@gmail.com>
2022-02-13 15:52:39 +0530

    Time: 76 ms (5.10%), Space: 14.1 MB (82.34%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def subsets(pos, sub):
+            if pos >= len(nums):
+                res.append(sub)
+                return
+            subsets(pos + 1, sub)
+            subsets(pos + 1, sub + [nums[pos]])
+        subsets(0, [])
+        return res
