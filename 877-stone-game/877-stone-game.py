15e636f33d51b5a3ab74eb0713aeb6c5b5aa8585
kalyan_yadav <kalyanyadav4321@gmail.com>
2022-02-14 09:13:04 +0530

    Time: 378 ms (46.77%), Space: 35.8 MB (33.36%) - LeetHub

--- 877-stone-game/877-stone-game.py
+++ 877-stone-game/877-stone-game.py
@@ -1,3 +1,21 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        dp = [[[0,0] for i in range(len(piles))] for i in range(len(piles) + 1)]
+        i = 0
+        col_inc = 0
+        row = len(piles)
+        while i < row:
+            j = col_inc
+            while j < len(piles):
+                
+                if dp[i+1][j][1] + piles[i] < dp[i][j-1][1] + piles[j]:
+                    dp[i][j][0] = dp[i][j-1][1] + piles[j]
+                    dp[i][j][1] = dp[i+1][j][0]
+                else:
+                    dp[i][j][0] = dp[i+1][j][1] + piles[i]
+                    dp[i][j][1] = dp[i][j-1][1]
+                i += 1
+                j += 1
+            col_inc += 1
+            row -= 1
+        return dp[-1][-1][0] >= dp[-1][-1][1]
