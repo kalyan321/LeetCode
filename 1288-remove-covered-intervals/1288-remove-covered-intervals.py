be369a4f6858900565a73b3e819e2d553ec508b6
kalyan_yadav <kalyanyadav4321@gmail.com>
2022-02-20 20:21:04 +0530

    Time: 96 ms (85.97%), Space: 14.5 MB (86.23%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.py
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -4,9 +4,7 @@ class Solution:
         pos = 0
         count = 1
         for i in range(1, len(intervals)):
-            if intervals[i][0] >= intervals[pos][0] and intervals[i][1] <= intervals[pos][1]:
-                pass
-            else:
+            if intervals[i][0] < intervals[pos][0] or intervals[i][1] > intervals[pos][1]:
                 pos = i
                 count += 1
         return count
