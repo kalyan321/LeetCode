7731d39232adbe1788ea75a7015d7b8b08c84984
kalyan_yadav <kalyanyadav4321@gmail.com>
2022-03-09 18:43:09 +0530

    Time: 2132 ms (12.22%), Space: 36.4 MB (61.12%) - LeetHub

---
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+class Solution:
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        
+        def dfs(node, v):
+            if node != v:
+                self.g[node].append(v)
+            self.visited[v] = True
+            for nei in self.d[v]:
+                if not self.visited[nei]:
+                    dfs(node, nei)
+        self.d = defaultdict(list)
+        self.g = defaultdict(list)
+        for edge in edges:
+            u, v = edge[0], edge[1] 
+            self.d[v].append(u)
+        for i in range(n):
+            self.visited = [False] * n
+            dfs(i, i)
+        return [sorted(self.g[i]) for i in range(n)]
